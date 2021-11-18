628930f848c6ac91433642e4b0c30c3b701e30a7
Vaibhav Kulshrestha <vaibhav1kulshrestha@gmail.com>
2021-11-17 18:19:04 -0700

    Use StratifiedKFold to train the CNN

---
+++ cnn_new.py
@@ -0,0 +1,124 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras import backend as K
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, LeakyReLU, MaxPooling2D
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.metrics import classification_report
+from sklearn.model_selection import StratifiedKFold
+
+
+# baseline model
+def train_model():
+    # define per-fold score containers
+    acc_per_fold = []
+    loss_per_fold = []
+
+    # define the K-fold Cross Validator
+    kfold = StratifiedKFold(n_splits=5, shuffle=True)
+
+    # K-fold Cross Validation model evaluation
+    fold_no = 1
+    for train, test in kfold.split(X_train, y_train_encoded):
+        # Define the model architecture
+        model = Sequential()
+        model.add(Conv2D(1, kernel_size=2,
+                  activation="relu", input_shape=(2, 4, 1)))
+        model.add(Flatten())
+        model.add(Dense(1, input_shape=(2, 4, 1)))
+        model.add(LeakyReLU(alpha=0.05))
+
+        # compile the model
+        model.compile(
+            loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"]
+        )
+
+        # generate a print
+        print(
+            "------------------------------------------------------------------------"
+        )
+        print(f"Training for fold {fold_no} ...")
+
+        # Fit data to model
+        history = model.fit(
+            X_train[train], y_train_encoded[train], batch_size=32, epochs=20, verbose=1,
+        )
+
+        # Generate generalization metrics
+        scores = model.evaluate(
+            X_train[test], y_train_encoded[test], verbose=0)
+        print(
+            f"Score for fold {fold_no}: {model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1]*100}%"
+        )
+        acc_per_fold.append(scores[1] * 100)
+        loss_per_fold.append(scores[0])
+
+        # Increase fold number
+        fold_no += 1
+
+    # provide the average scores
+    print("\n------------------------------------------------------------------------")
+    print("Score per fold")
+    for i in range(0, len(acc_per_fold)):
+        print(
+            "------------------------------------------------------------------------"
+        )
+        print(
+            f"> Fold {i+1} - Loss: {loss_per_fold[i]} - Accuracy: {acc_per_fold[i]}%")
+    print("------------------------------------------------------------------------")
+    print("Average scores for all folds:")
+    print(f"> Accuracy: {np.mean(acc_per_fold)} (+- {np.std(acc_per_fold)})")
+    print(f"> Loss: {np.mean(loss_per_fold)}")
+    print("------------------------------------------------------------------------")
+
+
+# read the datasets
+X_train = pd.read_csv("./X_train_clean.csv")
+X_test = pd.read_csv("./X_test_clean.csv")
+y_train = pd.read_csv("./y_train.csv")
+y_test = pd.read_csv("./y_test.csv")
+
+# reshape the datasets for CNN
+X_train.drop("case_id", inplace=True, axis=1)
+tmp = np.array(X_train)
+tmp = tmp.reshape(-1, 2, 4)
+X_train = pd.DataFrame(sum(map(list, tmp), []))
+tmp = []
+for i, g in X_train.groupby(np.arange(len(X_train)) // 2):
+    tmp.append(g)
+tmp = np.array([i.to_numpy() for i in tmp])
+# the number of images, shape of the image, and the number of channels
+X_train = tmp.reshape(402, 2, 4, 1)
+X_test.drop("case_id", inplace=True, axis=1)
+tmp = np.array(X_test)
+tmp = tmp.reshape(-1, 2, 4)
+X_test = pd.DataFrame(sum(map(list, tmp), []))
+tmp = []
+for i, g in X_test.groupby(np.arange(len(X_test)) // 2):
+    tmp.append(g)
+tmp = np.array([i.to_numpy() for i in tmp])
+# the number of images, shape of the image, and the number of channels
+X_test = tmp.reshape(101, 2, 4, 1)
+
+# encode class values as integers
+encoder = LabelEncoder()
+encoder.fit(y_train.vital_status)
+y_train_encoded = encoder.transform(y_train.vital_status)
+encoder = LabelEncoder()
+encoder.fit(y_test.vital_status)
+y_test_encoded = encoder.transform(y_test.vital_status)
+
+# normalize the test and train data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train.reshape(-1, X_train.shape[-1])).reshape(
+    X_train.shape
+)
+X_test = scaler.transform(
+    X_test.reshape(-1, X_test.shape[-1])).reshape(X_test.shape)
+
+
+# evaluate the model with standardized dataset
+train_model()
