49b360c9a5098fa8135e6158195c17542a558159
Vaibhav Kulshrestha <vaibhav1kulshrestha@gmail.com>
2021-11-23 18:22:12 -0700

    Add ylabel and yticks to the accuracy scatter plot

--- plot_acc.py
+++ plot_acc.py
@@ -1,6 +1,7 @@
+import math
 import matplotlib.pyplot as plt
 
-x = [1, 2, 3, 25, 30]
+x = [1, 2, 4, 25, 30]
 y = [
     41.152474880218506,
     52.0476231406886,
@@ -19,7 +20,11 @@ n = [
 fig, ax = plt.subplots()
 plt.tick_params(axis="x", which="both", bottom=False,
                 top=False, labelbottom=False)
-ax.scatter(x, y)
+plt.hlines(y=58.4158415842, xmin=1, xmax=30,
+           label="Baseline", color="r", linestyle="-")
+plt.ylabel("Accuracy (%)")
+plt.yticks(range(math.ceil(max(y)) + 1))
+ax.scatter(x, y, color=["red", "red", "green", "green", "green"])
 
 for i, txt in enumerate(n):
     ax.annotate(txt, (x[i], y[i]))
